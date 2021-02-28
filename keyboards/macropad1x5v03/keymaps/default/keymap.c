48fea0cd8b804352dd5bbf0ecc075c727e17f677
Leon S <leonstubbig@web.de>
2021-02-28 11:27:54 +0100

    V03 revision of macropad1x5

---
+++ keyboards/macropad1x5v03/keymaps/default/keymap.c
@@ -0,0 +1,47 @@
+/* Copyright 2020 Leon Stubbig <leonstubbig@web.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _Media,
+    _BL,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_Media] = LAYOUT(
+        _______, LCTL(LSFT(LALT(KC_M))), KC_MPRV, KC_MNXT, KC_MPLY
+    ),
+    [_BL] = LAYOUT(
+        _______, _______, BL_DEC, BL_INC, BL_BRTG
+    ),
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_AUDIO_VOL_UP);
+        } else {
+            tap_code(KC_AUDIO_VOL_DOWN);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_AUDIO_VOL_UP);
+        } else {
+            tap_code(KC_AUDIO_VOL_DOWN);
+        }
+    }
+}
